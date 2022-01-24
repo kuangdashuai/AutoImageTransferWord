e915b941b827d94cdf2dbbff89504fa4f6d6439d
旷大帅-华硕 <979776446@qq.com>
2022-01-24 15:55:12 +0800

    图片识别文字

---
+++ AutoImageTransferWord.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import pytesseract
+if __name__ == '__main__':
+    pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files\Tesseract-OCR\tesseract.exe'
+    # 文字识别是ORC的一部分内容，ORC的意思是光学字符识别，通俗讲就是文字识别。Tesseract是一个用于文字识别的工具，我们结合Python使用可以很快的实现文字识别
+    image_path = 'C:\\Users\\kuangdashuai\\Desktop\\无标题.png'
+    dir_config = '--tessdata-dir "D:\\Program Files\\Tesseract-OCR\\tessdata"'
+    # chi_sim是简体中文
+    text = pytesseract.image_to_string(Image.open(image_path),
+                                       config=dir_config, lang='chi_sim')
+    #设置为英文或阿拉伯字母的识别
+    # text = pytesseract.image_to_string(Image.open(image_path), lang='eng')
+
+    print(text)
+    f = open('output001.txt', 'w')
+    f.write(text)
+    f.close()
